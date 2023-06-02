8d3352b3476887184c9f2a2457a88020b63e8c88
shams220 <103259015+shams220@users.noreply.github.com>
2023-06-02 21:12:22 +0530

    newone

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ let n  = document.querySelectorAll(".drum").length
 for(let i = 0; i<n ;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
         
-        let buttoninnerHTML = this.buttoninnerHTML
+        let buttonInnerHTML = this.buttoninnerHTML
 
-        makeSound(buttoninnerHTML)
-        buttonAnimation(buttoninnerHTML)
+        makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
 
        
     })
@@ -41,40 +41,40 @@ buttonAnimation(event.key)
 function makeSound(key){
     switch (key) {
         case "w":
-            let audio1 = new Audio('sounds/tom-1.mp3')
+            let audio1 = new Audio("sounds/tom-1.mp3")
             audio1.play();
             break;
 
             case "a":
 
-            let audio2 = new Audio('sounds/tom-2.mp3')
+            let audio2 = new Audio("sounds/tom-2.mp3")
             audio2.play();
             break;
 
             case "s":
 
-            let audio3 = new Audio('sounds/tom-3.mp3')
+            let audio3 = new Audio("sounds/tom-3.mp3")
             audio3.play()
             break;
            
 
             case "d":
-            let audio4 = new Audio('sounds/tom-4.mp3')
+            let audio4 = new Audio("sounds/tom-4.mp3")
             audio4.play();
             break;
 
             case "j":
-            let audio5 = new Audio('sounds/snare.mp3')
+            let audio5 = new Audio("sounds/snare.mp3")
             audio5.play();
             break;
 
             case "k":
-            let audio6 = new Audio('sounds/kick-bass.mp3')
+            let audio6 = new Audio("sounds/kick-bass.mp3")
             audio6.play();
             break;
 
             case "l":
-            let audio7 = new Audio('sounds/crash.mp3')
+            let audio7 = new Audio("sounds/crash.mp3")
             audio7.play();
             break;
 
@@ -86,12 +86,16 @@ function makeSound(key){
 }
 
 function buttonAnimation(currentKey){
-let activeButtin = document.querySelector("."+currentKey)
+
+let activeButton = document.querySelector("."+currentKey)
+
+activeButton.classList.add("pressed") 
 
 setTimeout(function(){
-    activeButton.classList.add("pressed") 
 
-},100)
+    activeButton.classList.remove("pressed") 
+
+}, 100)
 }
 // let audio = new Audio('sounds/tom-1.mp3')
         // audio.play()
